c67634208a88b417811dfc284a97707e658984c3
Yunes Mor, Juan Manuel <jyunesmor@gmail.com>
2022-09-01 19:53:10 -0300

    Segundo Commit

--- 10- Array/src/servicio/arrayservicio.java
+++ 10- Array/src/servicio/arrayservicio.java
@@ -3,52 +3,45 @@
  */
 package servicio;
 
-
 import entidad.array;
 import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class arrayservicio {
-    
-          double [] vecA = new double [4];
-          double [] vecB = new double [4];
-          double [] vector1 = new double [4];
-          double [] vector2 = new double [4];
-          DecimalFormat df = new DecimalFormat("#.00");
-    
-    public void llenarArray(array a){
+        DecimalFormat df = new DecimalFormat("#.00");
+        double[] vector1 = new double[50];
+        double[] vector2 = new double[20];
         
-        for (int i = 0; i < 4; i++) {
-//            Arrays.fill(vecA,i,i+1,Math.random()*10+1);
-//            a.setArregloA(vecA);
-             vector1[i]=((int)(Math.random() * 10));
-             a.setArregloA(vector1);
+
+    public void llenarArray(array a) {
+        
+        for (int i = 0; i < a.getArregloA().length; i++) {
+
+            vector1[i] =  Math.random() * 100 + 1;
+            
+            a.setArregloA(vector1);
         }
-        System.out.println(Arrays.toString(a.getArregloA()));
+        System.out.print(Arrays.toString(a.getArregloA()));
     }
 
-    public void ordenarArray(array a){
-     
-     
-            Arrays.sort(vector1);
-            a.setArregloA(vector1);
+    public void ordenarArray(array a) {
+
+        Arrays.sort(vector1);
+        a.setArregloA(vector1);
         System.out.println(Arrays.toString(a.getArregloA()));
-        
-    }    
-    
-    public void complatarArrayB(array a){
-//      int lonA = a.getArregloB().length/2;
-         
-        for (int i = 2; i < 4; i++) {
-//             vector2[] = Arrays.copyOf(vector1,2);
+
+    }
+
+    public void complatarArrayB(array a) {
+        int lonA = a.getArregloB().length / 2;
+       
+        System.arraycopy(vector1, 0, vector2, 0, lonA);
+        for (int i = lonA; i < a.getArregloB().length; i++) {
+            vector2[i] = 0.5;
         }
-        vector2 = Arrays.copyOf(vector1,2); 
-            
-        for (int i = 2; i < 4; i++) {
-            vector2[i]= 0.5;
-            a.setArregloB(vector2);
-           System.out.println(Arrays.toString(a.getArregloB()));
+        for (int i = 0; i < a.getArregloB().length; i++) {
+            System.out.print("- " + vector2[i] + " -");
         }
-    
-    }    
+        System.out.println("");
+    }
 }
